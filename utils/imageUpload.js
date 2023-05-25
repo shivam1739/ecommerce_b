0623a9111f428f78e8d8f632b310fba0198e0141
shivam1739 <patelshivam1739@gmail.com>
2023-05-25 14:36:37 +0530

    add changes of multer

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
     cb(null, "Images");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
